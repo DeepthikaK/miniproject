18d0df760f9ab417501acf47f2713b8bcfa4b6a6
Deepthika <DeepthikaK@users.noreply.github.com>
2017-07-05 06:16:17 +0530

    Add files via upload

---
+++ Miniproject/js/discussion.js
@@ -0,0 +1,22 @@
+$('#menu1').click(function(){
+	$('#main_body').html('');
+	$.ajax({
+		url:"discussion.php",
+		type:"post",
+		
+		dataType:"json",
+		success:function(data){
+			var i=0;
+			while(data.rows){
+				$('#main_body').append('<u>'+data[i].sender+'@'+data[i].date+'</u>:'+data[i].message+'<br><br>');
+				i++;
+				
+				
+			}
+		}
+		
+	});
+});
+function fun(){
+	location.href='logout.php';
+}
